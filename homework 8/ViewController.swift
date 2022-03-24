7a1f5826bcb4c50fbfc0bad9d463b332cd925766
mac <mac@MacBook-Pro-mac.local>
2022-03-24 20:23:44 +0300

    www

---
+++ homework 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 8
+//
+//  Created by mac on 24.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
